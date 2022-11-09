1bb06aecd5cc1888e5eca86b69e9e9692fef8d21
mzhu <mz2789@columbia.edu>
2022-11-08 22:43:21 -0500

    Update LeetCode.sql

--- LeetCode.sql
+++ LeetCode.sql
@@ -1,4 +1,4 @@
-/*
+
 # 579. Find Cumulative Salary of an Employee
 USE test;
 DROP TABLE Employee;
@@ -442,7 +442,7 @@ select team_id,count(*) as team_size
 FROM employee
 group by team_id) t
 ON e.team_id=t.team_id
-*/
+
 
 -- 1350. Students With Invalid Departments
 Create table If Not Exists Departments (id int, name varchar(30));
@@ -470,12 +470,383 @@ ON s.department_id=d.id
 WHERE d.id is null
 
 
+-- 1421. NPV Queries
+SELECT q.id,q.year,ifnull(npv,0) as npv
+FROM NPV n
+Right JOIN queries q
+ON n.id=q.id
+and n.year=q.year
+
+
+-- 1398. Customers Who Bought Products A and B but Not C
+Create table If Not Exists Customers (customer_id int, customer_name varchar(30));
+Create table If Not Exists Orders (order_id int, customer_id int, product_name varchar(30));
+Truncate table Customers;
+insert into Customers (customer_id, customer_name) values ('1', 'Daniel');
+insert into Customers (customer_id, customer_name) values ('2', 'Diana');
+insert into Customers (customer_id, customer_name) values ('3', 'Elizabeth');
+insert into Customers (customer_id, customer_name) values ('4', 'Jhon');
+Truncate table Orders;
+insert into Orders (order_id, customer_id, product_name) values ('10', '1', 'A');
+insert into Orders (order_id, customer_id, product_name) values ('20', '1', 'B');
+insert into Orders (order_id, customer_id, product_name) values ('30', '1', 'D');
+insert into Orders (order_id, customer_id, product_name) values ('40', '1', 'C');
+insert into Orders (order_id, customer_id, product_name) values ('50', '2', 'A');
+insert into Orders (order_id, customer_id, product_name) values ('60', '3', 'A');
+insert into Orders (order_id, customer_id, product_name) values ('70', '3', 'B');
+insert into Orders (order_id, customer_id, product_name) values ('80', '3', 'D');
+insert into Orders (order_id, customer_id, product_name) values ('90', '4', 'C');
+SELECT t.customer_id,t.customer_name
+FROM (
+select c.customer_id,c.customer_name,sum(o.product_name="A") as a,sum(o.product_name="B") as b,sum(o.product_name="C") as c
+from  Customers c 
+join Orders o 
+on c.customer_id=o.customer_id
+group by c.customer_id,c.customer_name
+having (a>0 and b>0 and C=0)
+)t
+
+-- 1445. Apples & Oranges
+DROP Table sales;
+Create table If Not Exists Sales (sale_date date, fruit ENUM('apples', 'oranges'), sold_num int);
+Truncate table Sales;
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-01', 'apples', '10');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-01', 'oranges', '8');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-02', 'apples', '15');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-02', 'oranges', '15');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-03', 'apples', '20');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-03', 'oranges', '0');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-04', 'apples', '15');
+insert into Sales (sale_date, fruit, sold_num) values ('2020-05-04', 'oranges', '16');
+Select *
+FROM sales
+
+
+-- 1479. Sales by Day of the Week
+Drop table Orders;
+Drop table Items;
+Create table If Not Exists Orders (order_id int, customer_id int, order_date date, item_id varchar(30), quantity int);
+Create table If Not Exists Items (item_id varchar(30), item_name varchar(30), item_category varchar(30));
+Truncate table Orders;
+insert into Orders (order_id, customer_id, order_date, item_id, quantity) values ('1', '1', '2020-06-01', '1', '10');
+insert into Orders (order_id, customer_id, order_date, item_id, quantity) values ('2', '1', '2020-06-08', '2', '10');
+insert into Orders (order_id, customer_id, order_date, item_id, quantity) values ('3', '2', '2020-06-02', '1', '5');
+insert into Orders (order_id, customer_id, order_date, item_id, quantity) values ('4', '3', '2020-06-03', '3', '5');
+insert into Orders (order_id, customer_id, order_date, item_id, quantity) values ('5', '4', '2020-06-04', '4', '1');
+insert into Orders (order_id, customer_id, order_date, item_id, quantity) values ('6', '4', '2020-06-05', '5', '5');
+insert into Orders (order_id, customer_id, order_date, item_id, quantity) values ('7', '5', '2020-06-05', '1', '10');
+insert into Orders (order_id, customer_id, order_date, item_id, quantity) values ('8', '5', '2020-06-14', '4', '5');
+insert into Orders (order_id, customer_id, order_date, item_id, quantity) values ('9', '5', '2020-06-21', '3', '5');
+Truncate table Items;
+insert into Items (item_id, item_name, item_category) values ('1', 'LC Alg. Book', 'Book');
+insert into Items (item_id, item_name, item_category) values ('2', 'LC DB. Book', 'Book');
+insert into Items (item_id, item_name, item_category) values ('3', 'LC SmarthPhone', 'Phone');
+insert into Items (item_id, item_name, item_category) values ('4', 'LC Phone 2020', 'Phone');
+insert into Items (item_id, item_name, item_category) values ('5', 'LC SmartGlass', 'Glasses');
+insert into Items (item_id, item_name, item_category) values ('6', 'LC T-Shirt XL', 'T-shirt');
+with t1 as (
+SELECT item_category,order_date,quantity,dayofweek(order_date) as wk
+FROM orders o
+right JOIN items i
+ON o.item_id=i.item_id)
+
+select t1.item_category as category, 
+sum(case 
+when t1.wk=2 
+THEN t1.quantity
+else 0
+end ) as 'Monday',
+sum(
+case 
+when t1.wk=3 THEN t1.quantity
+else 0
+end) as 'Tuesday',
+sum(
+case
+when t1.wk=4 THEN t1.quantity
+else 0
+end )
+as 'Wednesday',
+sum(
+case
+when t1.wk=5 THEN t1.quantity
+else 0 
+end) as 'Thursday',
+sum(
+case 
+when t1.wk=6 THEN t1.quantity
+else 0
+end) as 'Friday',
+sum(
+case
+when t1.wk=7 THEN t1.quantity
+else 0
+end 
+)as 'Saturday',
+sum(
+case
+when t1.wk=1 THEN t1.quantity
+else 0
+end) as 'Sunday'
+from t1
+group by t1.item_category
+
+-- 1596. The Most Frequently Ordered Products for Each Customer
+use test;
+DROP table Customers;
+DROP table Orders;
+DROP table Products;
+Create table If Not Exists Customers (customer_id int, name varchar(10));
+Create table If Not Exists Orders (order_id int, order_date date, customer_id int, product_id int);
+Create table If Not Exists Products (product_id int, product_name varchar(20), price int);
+Truncate table Customers;
+insert into Customers (customer_id, name) values ('1', 'Alice');
+insert into Customers (customer_id, name) values ('2', 'Bob');
+insert into Customers (customer_id, name) values ('3', 'Tom');
+insert into Customers (customer_id, name) values ('4', 'Jerry');
+insert into Customers (customer_id, name) values ('5', 'John');
+Truncate table Orders;
+insert into Orders (order_id, order_date, customer_id, product_id) values ('1', '2020-07-31', '1', '1');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('2', '2020-7-30', '2', '2');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('3', '2020-08-29', '3', '3');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('4', '2020-07-29', '4', '1');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('5', '2020-06-10', '1', '2');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('6', '2020-08-01', '2', '1');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('7', '2020-08-01', '3', '3');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('8', '2020-08-03', '1', '2');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('9', '2020-08-07', '2', '3');
+insert into Orders (order_id, order_date, customer_id, product_id) values ('10', '2020-07-15', '1', '2');
+Truncate table Products;
+insert into Products (product_id, product_name, price) values ('1', 'keyboard', '120');
+insert into Products (product_id, product_name, price) values ('2', 'mouse', '80');
+insert into Products (product_id, product_name, price) values ('3', 'screen', '600');
+insert into Products (product_id, product_name, price) values ('4', 'hard disk', '450');
+
+
+select c.customer_id,p.product_id,p.product_name,rank() over (partition by customer_id order by count(p.product_id) desc) as counts
+from customers c
+join orders o on c.customer_id=o.customer_id
+join products p on o.product_id=p.product_id
+group by c.customer_id,p.product_id,p.product_name
+
+-- 1613. Find the Missing IDs
+DROP table Customers;
+Create table If Not Exists Customers (customer_id int, customer_name varchar(20));
+Truncate table Customers;
+insert into Customers (customer_id, customer_name) values ('1', 'Alice');
+insert into Customers (customer_id, customer_name) values ('4', 'Bob');
+insert into Customers (customer_id, customer_name) values ('5', 'Charlie');
+
+WITH RECURSIVE nums AS (
+    SELECT 1 AS value
+    UNION ALL
+    SELECT value + 1 AS value
+    FROM nums
+    WHERE nums.value < (select max(customer_id) from Customers)
+)
+select value as ids from nums
+with recursive cte1 as(
+select 1 as num
+union all 
+select num+1 as value 
+from cte1
+where cte1.num<10)
+select * from cte1
+
+-- 1623. All Valid Triplets That Can Represent a Country
+Create table If Not Exists SchoolA (student_id int, student_name varchar(20));
+Create table If Not Exists SchoolB (student_id int, student_name varchar(20));
+Create table If Not Exists SchoolC (student_id int, student_name varchar(20));
+Truncate table SchoolA;
+insert into SchoolA (student_id, student_name) values ('1', 'Alice');
+insert into SchoolA (student_id, student_name) values ('2', 'Bob');
+Truncate table SchoolB;
+insert into SchoolB (student_id, student_name) values ('3', 'Tom');
+Truncate table SchoolC;
+insert into SchoolC (student_id, student_name) values ('3', 'Tom');
+insert into SchoolC (student_id, student_name) values ('2', 'Jerry');
+insert into SchoolC (student_id, student_name) values ('10', 'Alice');
+
+select a.student_name as member_A,b.student_name as member_B,c.student_name as member_C
+from SchoolA a
+join SchoolB b
+join SchoolC c
+on (a.student_id != b.student_id and a.student_id != c.student_id and b.student_id != c.student_id
+and a.student_name != b.student_name and a.student_name != c.student_name and b.student_name != c.student_name)
+
+
+-- 1777. Product's Price for Each Store
+DROP table products;
+
+Create table If Not Exists Products (product_id int, store ENUM('store1', 'store2', 'store3'), price int);
+Truncate table Products;
+insert into Products (product_id, store, price) values ('0', 'store1', '95');
+insert into Products (product_id, store, price) values ('0', 'store3', '105');
+insert into Products (product_id, store, price) values ('0', 'store2', '100');
+insert into Products (product_id, store, price) values ('1', 'store1', '70');
+insert into Products (product_id, store, price) values ('1', 'store3', '80');
+
+select product_id,
+sum(case when store='store1' then price
+
+end) as 'store1',
+sum(case when store='store2' then price
+
+end) as 'store2',
+sum(case when store='store3' then price
+
+end) as 'store3'
+from Products
+group by product_id
+
+-- 1972. First and Last Call On the Same Day
+Create table If Not Exists Calls (caller_id int, recipient_id int, call_time datetime);
+Truncate table Calls;
+insert into Calls (caller_id, recipient_id, call_time) values ('8', '4', '2021-08-24 17:46:07');
+insert into Calls (caller_id, recipient_id, call_time) values ('4', '8', '2021-08-24 19:57:13');
+insert into Calls (caller_id, recipient_id, call_time) values ('5', '1', '2021-08-11 05:28:44');
+insert into Calls (caller_id, recipient_id, call_time) values ('8', '3', '2021-08-17 04:04:15');
+insert into Calls (caller_id, recipient_id, call_time) values ('11', '3', '2021-08-17 13:07:00');
+insert into Calls (caller_id, recipient_id, call_time) values ('8', '11', '2021-08-17 22:22:22');
+
+WITH CTE AS (
+                SELECT caller_id AS user_id, call_time, recipient_id FROM Calls
+                UNION 
+                SELECT recipient_id AS user_id, call_time, caller_id AS recipient_id FROM Calls
+            ),
+CTE1 AS (
+        SELECT 
+        user_id,
+        recipient_id,
+        DATE(call_time) AS DAY,
+        DENSE_RANK() OVER(PARTITION BY user_id, DATE(call_time) ORDER BY call_time ASC) AS RN,
+        DENSE_RANK() OVER(PARTITION BY user_id, DATE(call_time) ORDER BY call_time DESC) AS RK
+        FROM CTE
+        )
+        
+SELECT user_id,DAY
+FROM CTE1
+WHERE RN = 1 OR RK = 1
+GROUP BY user_id, DAY
+HAVING COUNT(DISTINCT recipient_id) = 1
+
+-- 2329. Product Sales Analysis V
+select s.user_id,sum(s.quantity*p.price) as spending
+from Sales s
+join Product p 
+on s.product_id=p.product_id
+group by s.user_id 
+
+-- 2394. Employees With Deductions
+
+select t.employee_id
+from(
+select e.employee_id,e.needed_hours,
+(SUM(CEIL(IFNULL(TIMESTAMPDIFF(SECOND, L.in_time, L.out_time),0) / 60)) / 60) as time
+
+from Logs l 
+right join Employees e
+on l.employee_id=e.employee_id
+group by l.employee_id
+having time<e.needed_hours) t
+
+order by t.employee_id
+
+-- 2292. Products With Three or More Orders in Two Consecutive Years
+drop TABLE ORDERS;
+Create table If Not Exists Orders (order_id int, product_id int, quantity int, purchase_date date);
+Truncate table Orders;
+insert into Orders (order_id, product_id, quantity, purchase_date) values ('1', '1', '7', '2020-03-16');
+insert into Orders (order_id, product_id, quantity, purchase_date) values ('2', '1', '4', '2020-12-02');
+insert into Orders (order_id, product_id, quantity, purchase_date) values ('3', '1', '7', '2020-05-10');
+insert into Orders (order_id, product_id, quantity, purchase_date) values ('4', '1', '6', '2021-12-23');
+insert into Orders (order_id, product_id, quantity, purchase_date) values ('5', '1', '5', '2021-05-21');
+insert into Orders (order_id, product_id, quantity, purchase_date) values ('6', '1', '6', '2021-10-11');
+insert into Orders (order_id, product_id, quantity, purchase_date) values ('7', '2', '6', '2022-10-11');
+
+SELECT *
+FROM Orders O1
+JOIN Orders O2
+ON O1.product_id=O2.product_id and 
+YEAR(O1.purchase_date)=YEAR(O2.purchase_date)+1 
 
 
 
+SELECT O1.product_id,year(O1.purchase_date) AS Year1
+FROM Orders O1 JOIN Orders O2
+ON (O1.product_id=O2.product_id and YEAR(O1.purchase_date)=YEAR(O2.purchase_date)+1 )
+GROUP BY O1.product_id, Year1
+HAVING COUNT(O1.order_id)>=3
 
+-- 2238. Number of Times a Driver Was a Passenger
 
+Create table If Not Exists Rides (ride_id int, driver_id int, passenger_id int);
+Truncate table Rides;
+insert into Rides (ride_id, driver_id, passenger_id) values ('1', '7', '1');
+insert into Rides (ride_id, driver_id, passenger_id) values ('2', '7', '2');
+insert into Rides (ride_id, driver_id, passenger_id) values ('3', '11', '1');
+insert into Rides (ride_id, driver_id, passenger_id) values ('4', '11', '7');
+insert into Rides (ride_id, driver_id, passenger_id) values ('5', '11', '7');
+insert into Rides (ride_id, driver_id, passenger_id) values ('6', '11', '3');
 
+ 
+select r.driver_id,count(t.passenger_id)
+from 
+(select distinct driver_id
+from Rides) r
+
+left join Rides t
+
+on r.driver_id=t.passenger_id
+
+group by r.driver_id
+
+-- 2228. Users With Two Purchases Within Seven Days
+Create table If Not Exists Purchases (purchase_id int, user_id int, purchase_date date);
+Truncate table Purchases;
+insert into Purchases (purchase_id, user_id, purchase_date) values ('4', '2', '2022-03-13');
+insert into Purchases (purchase_id, user_id, purchase_date) values ('1', '5', '2022-02-11');
+insert into Purchases (purchase_id, user_id, purchase_date) values ('3', '7', '2022-06-19');
+insert into Purchases (purchase_id, user_id, purchase_date) values ('6', '2', '2022-03-20');
+insert into Purchases (purchase_id, user_id, purchase_date) values ('5', '7', '2022-06-19');
+insert into Purchases (purchase_id, user_id, purchase_date) values ('2', '2', '2022-06-08');
+
+SELECT P1.user_id
+FROM Purchases P1
+JOIN Purchases P2
+ON P1.user_id=P2.user_id AND 
+P1.purchase_date BETWEEN p2.purchase_date AND p2.purchase_date+7
+AND p1.purchase_id!=p2.purchase_id
+GROUP BY P1.user_id 
+ORDER BY P1.user_id 
+
+-- 2173. Longest Winning Streak
+Create table If Not Exists Matches (player_id int, match_day date, result ENUM('Win', 'Draw', 'Lose'));
+Truncate table Matches;
+insert into Matches (player_id, match_day, result) values ('1', '2022-01-17', 'Win');
+insert into Matches (player_id, match_day, result) values ('1', '2022-01-18', 'Win');
+insert into Matches (player_id, match_day, result) values ('1', '2022-01-25', 'Win');
+insert into Matches (player_id, match_day, result) values ('1', '2022-01-31', 'Draw');
+insert into Matches (player_id, match_day, result) values ('1', '2022-02-08', 'Win');
+insert into Matches (player_id, match_day, result) values ('2', '2022-02-06', 'Lose');
+insert into Matches (player_id, match_day, result) values ('2', '2022-02-08', 'Lose');
+insert into Matches (player_id, match_day, result) values ('3', '2022-03-30', 'Win');
+
+SELECT player_id, IFNULL(MAX(cnt), 0) AS longest_streak
+FROM Matches m
+LEFT JOIN (
+	SELECT player_id, p_rnk - rnk AS diff, COUNT(p_rnk - rnk) AS cnt
+	FROM (
+		SELECT *, 	
+			CAST(RANK () OVER (PARTITION BY player_id, result ORDER BY match_day) AS SIGNED) AS rnk,
+			CAST(RANK () OVER (PARTITION BY player_id ORDER BY match_day) AS SIGNED) AS p_rnk
+		FROM Matches
+	) t
+	WHERE result = 'Win'
+	GROUP BY player_id, diff
+) t2
+	USING (player_id)
+GROUP BY player_id
 
 
 
